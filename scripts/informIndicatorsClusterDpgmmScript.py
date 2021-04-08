78283033fdb3eee30290466c22d1b41fbc9f362f
achutman <56025467+achutman@users.noreply.github.com>
2021-04-08 14:19:02 +0100

    Update informIndicatorsClusterDpgmmScript.py

--- scripts/informIndicatorsClusterDpgmmScript.py
+++ scripts/informIndicatorsClusterDpgmmScript.py
@@ -5,6 +5,12 @@ Created on Thu 08 April 2021
 @author: Achut Manandhar, Data Scientist, Migration and Displacement Initiative, Save the Children International
 Contact: William Low, Project Lead, Migration and Displacement Initiative, Save the Children International
 
+This work was done in collaboration with University of Virginia
+Contact:
+Michele Claibourn, DIRECTOR OF SOCIAL, NATURAL, ENGINEERING AND DATA SCIENCES, UVA LIBRARY
+David Leblang, PROFESSOR OF POLITICS AND PUBLIC POLICY, DIRECTOR OF THE GLOBAL POLICY CENTER, Frank Batten School of Leadership and Public Policy
+
+
 Cluster INFORM indices for a particular year 2020
 using Dirichlet Process Gaussian Mixture Model (DPGMM)
 https://scikit-learn.org/stable/modules/mixture.html
